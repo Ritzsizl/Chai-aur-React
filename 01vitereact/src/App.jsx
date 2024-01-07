6684cd401586857522f5196466c7b9f2c7146a4e
Ritzsizl <ritzraha4u@gmail.com>
2024-01-07 15:51:53 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 
 import Chai from "./chai"
 function App() {
+  const username = "chai aur code"
   return (
    <>
    <Chai/>
-   <h1>Chai aur React</h1>
+   <h1>Chai aur React {username}</h1>
    <p>test para</p>
    </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App | Chai</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank',
+//     },
+//     children: 'Click me to visit Google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "chai aur react"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google',
+    anotherElement
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  
-    <App />
+    // <MyApp />
+    // anotherElement
+    reactElement
+    // <App/>
 
 )
